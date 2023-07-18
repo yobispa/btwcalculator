230c432c96f2b7bde50ecde54c2f044bd6dc7d4f
Yoandel Obispa <302631801@student.rocmondriaan.nl>
2023-07-18 18:37:00 +0200

    added some functions

--- js/main.js
+++ js/main.js
@@ -15,16 +15,17 @@ function inputValidate() {
         calculate(value);
     }
 }
-function calculate(netto) {
-    const totalValueWithTax = netto * 1.21;
-    const taxValue = totalValueWithTax - netto;
-    const youngOnesProfit = netto / 100 * 2.9;
-    const yearlyTaxes = netto - youngOnesProfit
+function calculate(amount) {
+    const totalValueWithTax = amount * 1.21;
+    const taxValue = totalValueWithTax - amount;
+    const youngOnesProfit = totalValueWithTax / 100 * 2.9;
+    const youngOnesProfitTax = youngOnesProfit / 100 * 121;
+    const yearlyTaxes = amount - youngOnesProfitTax
     const yearlyTaxesProfit = yearlyTaxes / 100 * 10;
     const userProfit = yearlyTaxes - yearlyTaxesProfit;
     
-document.querySelector(".total-outcome").textContent = `Totaal bedrag met 21% btw: €${totalValueWithTax.toFixed(2)}`
-document.querySelector(".young-ones-profit").textContent = `Bedrag die YoungOnes profiteer: €${youngOnesProfit.toFixed(2)}`
-document.querySelector(".yearly-tax").textContent = `Bedrag die voor inkomstenbelasting moet bewaren: €${yearlyTaxesProfit.toFixed(2)}%`
+document.querySelector(".total-outcome").innerHTML = `Totaal bedrag met 21% btw: €${totalValueWithTax.toFixed(2)} <br><br> bedrag btw: €${taxValue.toFixed(2)}`
+document.querySelector(".young-ones-profit").textContent = `Bedrag die YoungOnes moet krijgen: €${youngOnesProfitTax.toFixed(2)}`
+document.querySelector(".yearly-tax").textContent = `Bedrag die voor inkomstenbelasting moet bewaren: €${yearlyTaxesProfit.toFixed(2)}`
 document.querySelector(".user-profit").textContent = `Jouw nieuwe bedrag: €${userProfit.toFixed(2)}`
 }
